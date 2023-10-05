245fb07f6fb725562613fc937caecb8d47f1a5f4
Milan Hauth <milahu@gmail.com>
2023-10-05 12:14:29 +0200

    up

--- src/wrap.sh
+++ src/wrap.sh
@@ -3,15 +3,11 @@
 # wrap text file at 78 columns
 
 # https://github.com/jgm/pandoc/issues/9122 # add input format plain
+# https://pandoc.org/custom-readers#example-plain-text-reader
 
 if ! [ -e "$1" ]; then
   echo usage: wrap.sh input_file.txt
   exit 1
 fi
 
-# the conversion done by pandoc is lossy
-# so we need a backup
-bak_path="$1.$(date +%F.%H-%M-%S)"
-cp -v "$1" "$bak_path"
-
-pandoc "$1" -t plain --wrap=auto --columns=78 -o "$1"
+pandoc "$1" -f plain.lua -t plain --wrap=auto --columns=78 -o "$1"
